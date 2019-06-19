01fd7ab0ddd059c4f8ec4b41024b4ba716014a4f
Christopher Watson <christopher_watson@icloud.com>
2019-06-19 14:35:51 -0400

    landing background, some bio

--- src/components/form.js
+++ src/components/form.js
@@ -9,7 +9,7 @@ const styles = {
     justifyContent: 'center',
     padding: '50px 0',
     backgroundColor: '#252525',
-    backgroundImage: `url("data:image/svg+xml,%3Csvg width='32' height='64' viewBox='0 0 32 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 28h20V16h-4v8H4V4h28v28h-4V8H8v12h4v-8h12v20H0v-4zm12 8h20v4H16v24H0v-4h12V36zm16 12h-4v12h8v4H20V44h12v12h-4v-8zM0 36h8v20H0v-4h4V40H0v-4z' fill='%23323232' fill-opacity='0.68' fill-rule='evenodd'/%3E%3C/svg%3E")`
+    backgroundImage: `url("data:image/svg+xml,%3Csvg width='12' height='16' viewBox='0 0 12 16' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4 .99C4 .445 4.444 0 5 0c.552 0 1 .45 1 .99v4.02C6 5.555 5.556 6 5 6c-.552 0-1-.45-1-.99V.99zm6 8c0-.546.444-.99 1-.99.552 0 1 .45 1 .99v4.02c0 .546-.444.99-1 .99-.552 0-1-.45-1-.99V8.99z' fill='%23323232' fill-opacity='0.86' fill-rule='evenodd'/%3E%3C/svg%3E")`
   },
   formCont: {
     display: 'flex',
