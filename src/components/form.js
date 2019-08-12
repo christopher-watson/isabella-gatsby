1dd5053bfadc726e24fd8e72070a14d0705a7f93
bisabella37 <51177524+bisabella37@users.noreply.github.com>
2019-08-12 17:28:19 -0400

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -225,6 +225,7 @@ class Form extends Component {
                   <button className='invalid-form-button' type="submit" disabled >Send</button>
                 }
               </div>
+render() {
  return (
       <div className="form" style={styles.formDiv}>
         <div className="form-container" style={styles.formCont}>
