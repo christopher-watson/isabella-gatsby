91096543279d7d54249ecf6f54ae8f4db1d80da9
Christopher Watson <christopher_watson@icloud.com>
2019-08-15 17:41:31 -0400

    ldjson3

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,6 +26,62 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
+  const jsonLD = {
+      "@context": "schema.org",
+      "@type": "LocalBusiness",
+      "name": "Isabella Fitness",
+      "image": "https://isabellafitness.com/wp-content/uploads/2016/10/Isbella-Logo-fit-green-up.png",
+      "@id": "",
+      "url": "isabellafitness.com",
+      "telephone": "732-532-3337",
+      "priceRange": "$$",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "242 Main Street #B-D",
+        "addressLocality": "Sayreville",
+        "addressRegion": "NJ",
+        "postalCode": "08872",
+        "addressCountry": "US"
+      },
+      "geo": {
+        "@type": "GeoCoordinates",
+        "latitude": 40.4684293,
+        "longitude": -74.36053379999998
+      },
+      "openingHoursSpecification": [{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday"
+        ],
+        "opens": "05:30",
+        "closes": "21:00"
+      },{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Friday",
+        "opens": "05:30",
+        "closes": "19:00"
+      },{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Saturday",
+        "opens": "08:30",
+        "closes": "12:00"
+      },{
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Sunday",
+        "opens": "08:00",
+        "closes": "12:00"
+      }],
+      "sameAs": [
+        "https://www.facebook.com/isabellafitness/",
+        "https://www.instagram.com/isabella_fitness_/",
+        "https://www.youtube.com/user/Bennyboi37",
+        "https://twitter.com/IsabellaFitness"
+      ]
+    }
+    
 
   return (
     <Helmet
@@ -68,7 +124,11 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <script type='application/ld+json'>
+        {JSON.stringify(jsonLD)}
+      </script>
+    </Helmet>
   )
 }
 
