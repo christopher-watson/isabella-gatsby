a515fe4f238937a32984d48cc994c900b66d92ce
bisabella37 <51177524+bisabella37@users.noreply.github.com>
2020-02-27 07:13:58 -0500

    Update slide.js

--- src/components/slide.js
+++ src/components/slide.js
@@ -117,8 +117,8 @@ export default class Slide extends Component {
                   <ul>
       {/* <li>Drop In - $25/class</li>
                     <li>10 Class Pass - $200</li> */}
-                    <li>Unlimited - $164/mo</li>
-                    <li>2x/wk - $139/mo</li>
+                    <li>Unlimited or 2x/wk </li>
+                    <li>Contact us for pricing</li>
                     <li>*Pricing is month to month</li>
                     <li>Drop-ins Contact us</li>
                   </ul>
@@ -144,8 +144,8 @@ export default class Slide extends Component {
                   <ul>
                    {/* <li>Drop In - $25/class</li>
                     <li>10 Class Pass - $200</li> */}
-                    <li>Unlimited - $149/mo</li>
-                    <li>2x/wk - $129/mo</li>
+                    <li>Unlimited or 2x/wk </li>
+                    <li>Contact us for pricing</li>
                     <li>*Pricing is month to month</li>
                   </ul>
                 </div>
