0beafa3c9394497b52200e3dd14a50e3439cd185
Christopher Watson <christopherwatson@ChristophersMBP.fios-router.home>
2021-02-15 17:33:52 -0500

    fix form validation

--- src/components/image.js
+++ src/components/image.js
@@ -15,17 +15,17 @@ import Img from "gatsby-image"
 
 const Image = () => (
   <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
+    // query={graphql`
+    //   query {
+    //     placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+    //       childImageSharp {
+    //         fluid(maxWidth: 300) {
+    //           ...GatsbyImageSharpFluid
+    //         }
+    //       }
+    //     }
+    //   }
+    // `}
     render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
   />
 )
